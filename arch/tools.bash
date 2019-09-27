fdc1a986771bb64fa7b7d462ffb9e0feeff62ce6
akarokr <luiz.moura.n@gmail.com>
2019-09-27 13:52:18 -0300

    mods

--- arch/tools.bash
+++ arch/tools.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(zsh curl xclip spotify google-chrome zip unzip docker megasync feh)
+tools=(zsh curl xclip spotify google-chrome zip unzip docker megasync net-tools)
 
 for i in ${tools[@]}; do
   yay -Syu --needed --noconfirm ${i}
