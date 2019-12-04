7cd1b1ebbf7610ab761b23e45cfd785bb93d72b4
Luiz Neto <luiz.neto@soaexpert.com.br>
2019-12-04 19:51:52 -0200

    I'm going through changes

--- arch/utils.zsh
+++ arch/utils.zsh
@@ -7,26 +7,29 @@ chsh -s $(which zsh)
 git clone https://github.com/girishrau/oh-my-zsh-customizations.git /tmp/minimal-theme
 
 #virtualenvwrapper
-/usr/bin/pip3 install --user virtualenvwrapper
+#/usr/bin/pip3 install --user virtualenvwrapper
 
 #conky
-mkdir $HOME/.conky 
-cp $HOME/dotfiles/conky/* $HOME/.conky/
-
-#i3
-mv $HOME/.i3/config $HOME/.i3/config_orig 
-cp $HOME/dotfiles/i3/config $HOME/.i3/
+#mkdir $HOME/.conky 
+#cp $HOME/dotfiles/conky/* $HOME/.conky/
+#
+##i3
+#mv $HOME/.i3/config $HOME/.i3/config_orig 
+#cp $HOME/dotfiles/i3/config $HOME/.i3/
 
 #install vim pathogen
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle 
-curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cp -r $HOME/dotfiles/vim/colors $HOME/.vim/
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 #vimrc
 cp $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 
+#vimrc theme
+mkdir $HOME/.vimrc/colors
+cp  $HOME/dotfiles/vim/themes/dotshare.vim $HOME/.vimrc/colors
+
 #screen setup
-cp $HOME/dotfiles/arch/screen_startup.bash $HOME
+#cp $HOME/dotfiles/arch/screen_startup.bash $HOME
 
 #oh my zsh
 if ! [ -d $HOME/.oh-my-zsh ]; then
@@ -39,6 +42,6 @@ mv $HOME/.zshrc $HOME/.zshrc_orig
 cp $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 
 #Xresourses
-mv $HOME/.Xresources .Xresources_orig 
-cp $HOME/dotfiles/x/Xresources .Xresources
+#mv $HOME/.Xresources .Xresources_orig 
+#cp $HOME/dotfiles/x/Xresources .Xresources
 
